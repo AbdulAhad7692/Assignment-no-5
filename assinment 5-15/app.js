28bb074bbebb043382b3b8a35cc82fca074ec07b
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-02-11 01:24:54 +0500

    Add files via upload

---
+++ assinment 5-15/app.js
@@ -0,0 +1,62 @@
+//___QUESTION NO 1___//
+
+// var student = []
+
+//___QUESTION NO 2__//
+
+// var object = []
+
+//__QUESTION NO 3__//
+
+// var array = ["Hello"]
+// var array = ["1234"]
+// var array = [true]
+// var array = ["hello" , 1234 ,true,]
+
+//__QUESTION NO 4__//
+
+// var Qualification = ["SSC", "HSC", "BCS", "BS", "BCOM", "Ms", "M. Phil.", "PhD"]
+
+// document.write("<h3>Qualifications : </h3>")
+// document.write(Qualification[0])
+// document.write("<br>")
+// document.write(Qualification[1])
+// document.write("<br>")
+// document.write(Qualification[2])
+// document.write("<br>")
+// document.write(Qualification[3])
+// document.write("<br>")
+// document.write(Qualification[4])
+// document.write("<br>")
+// document.write(Qualification[5])
+// document.write("<br>")
+// document.write(Qualification[6])
+// document.write("<br>")
+// document.write(Qualification[7])
+// document.write("<br>")
+
+//__QUESTION NO 5__//
+
+// var students = ["Ahad", "Faiq", "Shahzaib"]
+// var marks = [480, 470, 465]
+
+// document.write(
+//     "score  of  " + students[0] + " is " + marks[0] + " whose percentage is " + marks[0] * 100 / 500
+// )
+// document.write("<br>")
+// document.write(
+//     "score  of  " + students[1] + " is " + marks[1] + " whose percentage is " + marks[1] * 100 / 500
+// )
+// document.write("<br>")
+// document.write(
+//     "score  of  " + students[2] + " is " + marks[2] + " whose percentage is " + marks[2] * 100 / 500
+// )
+// document.write("<br>")
+
+//__QUESTION NO 6__//
+
+var colors = ["red ", "yellow ", "green "]
+document.write(colors)
+document.write("<br>")
+var color = prompt("Enter color")
+document.write(color + " ," + colors)
