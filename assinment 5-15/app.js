3125e550c4d805c6a77d1509abc9748dbe0ecd7c
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-02-15 21:20:37 +0500

    Add files via upload

--- assinment 5-15/app.js
+++ assinment 5-15/app.js
@@ -13,7 +13,7 @@
 // var array = [true]
 // var array = ["hello" , 1234 ,true,]
 
-//__QUESTION NO 4__//
+//__QUESTION NO 7__//
 
 // var Qualification = ["SSC", "HSC", "BCS", "BS", "BCOM", "Ms", "M. Phil.", "PhD"]
 
@@ -35,7 +35,7 @@
 // document.write(Qualification[7])
 // document.write("<br>")
 
-//__QUESTION NO 5__//
+//__QUESTION NO 8__//
 
 // var students = ["Ahad", "Faiq", "Shahzaib"]
 // var marks = [480, 470, 465]
@@ -53,7 +53,7 @@
 // )
 // document.write("<br>")
 
-//__QUESTION NO 6__//
+//__QUESTION NO 9__//
 
 // var colors = ["red ", "yellow ", "green "]
 // document.write(colors)
@@ -76,3 +76,73 @@
 // var RemoveIndex2 = prompt("Enter Number", "2")
 // colors.splice(RemoveIndex + RemoveIndex2)
 // document.write("<br>" + colors)
+
+//__QUESTION NO 10__//
+
+// var score = [320, 230, 480, 120]
+// document.write("Score of Student : " + score + "<br>")
+// score.sort()
+// document.write( "Oredered Score of Students : " + score)
+
+//__QUESTION NO 11__//
+
+// var city = ["karachi ", "Lahore ", "Islamabad", "Quetta", "Peshawar "]
+// document.write("<h4>Cities List : </h4>")
+// document.write(city)
+// var selectedcities;
+// document.write("<h4>Selected Cites : </h4>")
+// document.write(city[2] + "," + city[3] )
+
+//__QUESTION NO 12__//
+
+// var cat = ["this", "is", "my", "cat"]
+// document.write(cat + "<br>")
+// document.write(cat.join(" "))
+
+//__QUESTION NO 13__//
+
+// var order = ["Keyboard","Mouse","Printer","Monitor"]
+
+// document.write("Devices: <br>" + order + "<br>")
+
+// document.write("Out: <br> " + order[0] + "<br>")
+// document.write("Out: <br> " + order[1] + "<br>")
+// document.write("Out: <br> " + order[2] + "<br>")
+// document.write("Out: <br> " + order[3] + "<br>")
+
+//__QUESTION NO 14__//
+
+// var order1 = ["Keyboard","Mouse","Printer","Monitor"]
+
+// document.write("Devices: <br>" + order1+ "<br>")
+
+// document.write("Out: <br> " + order1[3] + "<br>")
+// document.write("Out: <br> " + order1[2] + "<br>")
+// document.write("Out: <br> " + order1[1] + "<br>")
+// document.write("Out: <br> " + order1[0] + "<br>")
+
+//__QUESTION NO 15__//
+
+// var mobile = ["Apple", "Samsung", "Motorola", "Huawei", "Nokia", "Sony", "Haier"]
+// document.write(
+// "<select class=mobile>" +
+//     " <option>apple<option>" +
+//     " <option>Samsung<option>" +
+//     " <option>Motorola<option>" +
+//     " <option>Huawei<option>" +
+//     " <option>Nokia<option>" +
+//     " <option>Sony<option>" +
+//     " <option>Haier<option>" +
+// "</select>"    
+// )
+
+
+
+
+
+
+
+
+
+
+
